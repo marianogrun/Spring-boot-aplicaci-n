4ba4f4a60c8ddae780670035b17e4d999659f3a7
mgrunbla <mgrunbla@dskw72.usal.edu.ar>
2019-09-20 17:02:08 -0300

    segundo commit

---
+++ AplicationSpringBoot/src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,134 @@
+package com.example.demo.entity;
+
+import java.io.Serializable;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.Transient;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+public class User implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1369148652554782331L;
+	
+	@Id //el id generado para la fila en la base de datos 
+	@GeneratedValue(strategy = GenerationType.AUTO,generator = "native") //esta anotación y la de abajo generam el autoincremento en MySql
+	@GenericGenerator(name="native",strategy = "native")
+	private Long id;
+	
+	@Column //columna de la base de datos del id generado arriba 
+	private String firstName;
+	@Column //columna de la base de datos del id generado arriba
+	private String lastName;
+	@Column //columna de la base de datos del id generado arriba
+	private String email;
+	@Column //columna de la base de datos del id generado arriba
+	private String userName;
+	@Column //columna de la base de datos del id generado arriba
+	private String password;
+	
+	@Transient //este valor no se mapea en la base de datos. Es omitido por Hibernate
+	private String confirmPassword;
+	
+	@ManyToMany
+	@JoinTable(name="user_roles",
+				joinColumns = @JoinColumn(name="user_id"),
+				inverseJoinColumns = @JoinColumn(name="role_id")) //busca el id del objeto role y lo pone como foreign key
+	private Set<Role> roles;
+
+	public User(Long id) {
+		super();
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
+				+ ", userName=" + userName + ", password=" + password + ", confirmPassword=" + confirmPassword
+				+ ", roles=" + roles + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((confirmPassword == null) ? 0 : confirmPassword.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((roles == null) ? 0 : roles.hashCode());
+		result = prime * result + ((userName == null) ? 0 : userName.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (confirmPassword == null) {
+			if (other.confirmPassword != null) 
+				return false;
+		} else if (!confirmPassword.equals(other.confirmPassword))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (roles == null) {
+			if (other.roles != null)
+				return false;
+		} else if (!roles.equals(other.roles))
+			return false;
+		if (userName == null) {
+			if (other.userName != null)
+				return false;
+		} else if (!userName.equals(other.userName))
+			return false;
+		return true;
+	}
+	
+	
+	
+	
+	
+}
